dc09cd519ae36befd1bd6c61640c2aa947f0d0f5
Chris Puppe <chrispuppe@gmail.com>
2021-12-04 15:07:16 -0800

    callbackAndHigherOrder q7 working

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -185,15 +185,22 @@ var users = [
 // Do not edit the code above.
 
 // CODE HERE 
+const getUserById = (arr, id, cb) => {
+  for (i=0;i<arr.length;i++){
+    if (arr[i].id === id){
+      return cb(arr[i])
+    }
+  }
+}
 
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// getUserById(users, '16t', user => {
-//   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
-// })
+getUserById(users, '16t', user => {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
+})
 
 ////////// CHALLENGE //////////
 
